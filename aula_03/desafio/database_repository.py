1f2d899b7d3470a27a00b0ba28f7d4d884fd0108
Gabriel Linhares <linhares.gs96@gmail.com>
2023-12-14 20:44:53 -0300

    feat(aula_03/challenge): add DatabaseRepository class

---
+++ aula_03/desafio/database_repository.py
@@ -0,0 +1,24 @@
+from abc import ABC
+import sqlite3
+from typing import Any
+
+class DatabateRepository(ABC):
+    """Databse repository to call the main queries"""
+
+    def __init__(self, db_name, query):
+        self.query = query
+        self.db_name = db_name
+
+    def run_query(self, query: str, *params: Any):
+        connection = sqlite3.connect(self.db_name)
+        cursor = connection.cursor()
+        cursor.execute(query, params)
+        connection.commit()
+        connection.close()
+
+    def run_script(self, query: str):
+        connection = sqlite3.connect(self.db_name)
+        cursor = connection.cursor()
+        cursor.executescript(query)
+        connection.commit()
+        connection.close()
